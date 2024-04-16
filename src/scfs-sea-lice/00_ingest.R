e4bf153e33c1006303787f18a272f8042c24049c
colebrookson <cole.brookson@gmail.com>
2024-04-16 16:27:58 -0400

    working on the hash for the match df

--- src/scfs-sea-lice/00_ingest.R
+++ src/scfs-sea-lice/00_ingest.R
@@ -1,6 +1,8 @@
 #' AUTHOR: Cole B. Brookson
 #' DATE OF CREATION: 2024-03-29
 
+library(magrittr)
+
 # pull the fish data & site data
 fish_data <- pull_data(
     data_loc = paste0(
@@ -21,6 +23,20 @@ site_data <- pull_data(
     env_load = TRUE
 )
 
+# join the datasets into one
+scfs_data <- dplyr::left_join(
+    x = fish_data,
+    y = site_data,
+    by = "site_id",
+    suffix = c("", ".annoying_duplicate_column")
+) %>%
+    select(-ends_with(".annoying_duplicate_column"))
+readr::write_csv(
+    data.frame(
+        scfs_names = names(scfs_data)
+    ),
+    here::here("./data/scfs-sea-lice/scfs-scfs-names-match-sheet.csv")
+)
 # create relationship table for all Pharos fields
 pharos_df <- readr::read_csv(here::here("./data/dataset-template.csv"))
 names(pharos_df)
@@ -36,4 +52,8 @@ match_df <- data.frame(
         "Organism sex", "Dead or alive", "Health notes", "Life stage", "Age",
         "Mass", "Length"
     )
+)
+readr::write_csv(
+    match_df,
+    here::here("./data/scfs-sea-lice/scfs-pharos-names-match-sheet.csv")
 )
